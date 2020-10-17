82cae29a1bbd555a82ef39b8b30cb79a3a318813
JLA <63759042+Blu3DrAEm@users.noreply.github.com>
2020-10-17 17:38:59 -0600

    Version 1.3
    -added score system
    -game quits when player enters 5 palindromes

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,13 +1,22 @@
-# isPalindrome
-# version 1.2
-# josh anderson 10/15/2020
+# ispalindrome
+# checks to see if a word is a palindrome
+# Josh Anderson 10/15/2020
+# current version 1.3 (10/17/2020)
 
 list = []
+score = 0
+
+print('Enter 5 palindromes to win!')
 while True:
-        palindrome = input('Enter a word to see if it is a palindrome: ')
-        isPalindrome = palindrome.find(palindrome[::-1])==0
-        if isPalindrome == True:
-                list.append(palindrome)
-                print(isPalindrome, list)
-        else:
-                print('That word is not a palindrome. Try again')
+	palindrome = input('Enter a word to see if it is a palindrome: ')
+	isPalindrome = palindrome.find(palindrome[::-1])==0
+	if isPalindrome == True:
+		list.append(palindrome)
+		score = score + 1
+		print(isPalindrome, list)
+	else:
+		print('That word is not a palindrome. Try again')
+
+	if score == 5:
+		print('You win!')
+		exit()
